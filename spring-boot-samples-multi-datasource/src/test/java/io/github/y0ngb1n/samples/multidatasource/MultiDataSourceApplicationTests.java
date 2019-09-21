86bb44b0f83dd8f00be02326827c45ff91a9681f
yangbin <y0ngb1n@163.com>
2019-09-21 10:57:14 +0800

    chore(multi-datasource): clean code

--- spring-boot-samples-multi-datasource/src/test/java/io/github/y0ngb1n/samples/multidatasource/MultiDataSourceApplicationTests.java
+++ spring-boot-samples-multi-datasource/src/test/java/io/github/y0ngb1n/samples/multidatasource/MultiDataSourceApplicationTests.java
@@ -3,6 +3,7 @@ package io.github.y0ngb1n.samples.multidatasource;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +26,14 @@ public class MultiDataSourceApplicationTests {
   @Test
   public void testFooDataSource() throws SQLException {
     log.info("foo datasource runtime: {}", fooDataSource.getConnection());
-    assert null != fooDataSource.getConnection();
-    assert fooDataSource.getConnection().toString().contains("jdbc:h2:mem:foo");
+    Assert.assertNotNull(fooDataSource.getConnection());
+    Assert.assertTrue(fooDataSource.getConnection().toString().contains("jdbc:h2:mem:foo"));
   }
 
   @Test
   public void testBarDataSource() throws SQLException {
     log.info("bar datasource runtime: {}", barDataSource.getConnection());
-    assert null != barDataSource.getConnection();
-    assert barDataSource.getConnection().toString().contains("jdbc:h2:mem:bar");
+    Assert.assertNotNull(barDataSource.getConnection());
+    Assert.assertTrue(barDataSource.getConnection().toString().contains("jdbc:h2:mem:bar"));
   }
 }
